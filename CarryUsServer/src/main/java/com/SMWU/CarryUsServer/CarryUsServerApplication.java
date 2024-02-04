6951d2c6ce25a1721fbc66b64b78cb412ffb33fc
sung-silver <jinna0319@gmail.com>
2024-02-04 23:36:02 +0900

    [INIT] project setting

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/CarryUsServerApplication.java
@@ -0,0 +1,13 @@
+package com.SMWU.CarryUsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryUsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryUsServerApplication.class, args);
+	}
+
+}
