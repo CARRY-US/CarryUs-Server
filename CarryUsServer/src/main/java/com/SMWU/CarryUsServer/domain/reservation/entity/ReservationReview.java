b9f9b13751bb4ea242a468e24ac3a1f7414eec3b
sung-silver <jinna0319@gmail.com>
2024-02-20 18:03:20 +0900

    [FEAT] reservation 도메인 엔티티 설계

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/entity/ReservationReview.java
@@ -0,0 +1,24 @@
+package com.SMWU.CarryUsServer.domain.reservation.entity;
+
+import com.SMWU.CarryUsServer.global.entity.AuditingTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "RESERVATIONS_REVIEWS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ReservationReview extends AuditingTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationReviewId;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    private double reviewRating;
+
+}
