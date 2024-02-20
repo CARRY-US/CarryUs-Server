db489da4e21f572492a515ae50e9024872f4360d
sung-silver <jinna0319@gmail.com>
2024-02-20 21:37:42 +0900

    [FEAT] memberRepository 구현

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepoistory.java
@@ -0,0 +1,11 @@
+package com.SMWU.CarryUsServer.domain.member.repository;
+
+import com.SMWU.CarryUsServer.domain.member.entity.Member;
+import com.SMWU.CarryUsServer.domain.member.entity.enums.PlatformType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepoistory extends JpaRepository<Long, Member> {
+    Optional<Member> findByPlatformTypeAndPlatformId(PlatformType platformType, String PlatformId);
+}
