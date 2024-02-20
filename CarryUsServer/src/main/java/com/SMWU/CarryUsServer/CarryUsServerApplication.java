373edc6d987ab25b109ee8aac5bf0a6a0f4075eb
sung-silver <jinna0319@gmail.com>
2024-02-20 18:04:39 +0900

    [FEAT] AuditingTimeEntity에 따른 @EnableJpaAuditing 어노테이션 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/CarryUsServerApplication.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/CarryUsServerApplication.java
@@ -2,8 +2,10 @@ package com.SMWU.CarryUsServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CarryUsServerApplication {
 
 	public static void main(String[] args) {
