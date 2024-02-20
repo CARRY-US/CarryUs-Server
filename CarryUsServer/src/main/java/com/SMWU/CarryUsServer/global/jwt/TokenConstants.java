b0a811631e69d6155a860377e6f58da550424f82
sung-silver <jinna0319@gmail.com>
2024-02-21 00:15:30 +0900

    [FEAT] JwtTokenManager 구현

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/global/jwt/TokenConstants.java
@@ -0,0 +1,8 @@
+package com.SMWU.CarryUsServer.global.jwt;
+
+public abstract class TokenConstants {
+    public static final String ID_CLAIM = "id";
+    public static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    public static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    public static final String BEARER = "Bearer ";
+}
