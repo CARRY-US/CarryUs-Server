3950f13a9a5a4dcf13212fdb71df6a72ee1dcfb9
sung-silver <jinna0319@gmail.com>
2024-02-23 01:02:50 +0900

    [FEAT] getPlatformType 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/auth/service/vo/MemberSignUpVO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/auth/service/vo/MemberSignUpVO.java
@@ -7,14 +7,14 @@ import com.SMWU.CarryUsServer.domain.member.entity.enums.Role;
 import lombok.Builder;
 
 @Builder
-public record MemberSignUpVO(Long memberId, String platformId, String nickName, PlatformType platformType, Role role,
-                             String birthYear, String gender, String phoneNumber, String profileImg, AuthType authType) {
+public record MemberSignUpVO(Long memberId, String platformId, String nickname, PlatformType platformType, Role role,
+                             String phoneNumber, String profileImg, AuthType authType) {
 
     public static MemberSignUpVO of(Member member, PlatformType platformType, Role role, AuthType authtype) {
         return MemberSignUpVO.builder()
                 .memberId(member.getMemberId())
                 .platformId(member.getPlatformId())
-                .nickName(member.getNickname())
+                .nickname(member.getNickname())
                 .platformType(platformType)
                 .role(role)
                 .phoneNumber(member.getPhoneNumber())
