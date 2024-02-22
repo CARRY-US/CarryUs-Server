1f5f677c629e5f731137b85a185331f9296cffe4
sung-silver <jinna0319@gmail.com>
2024-02-23 01:02:50 +0900

    [FEAT] getPlatformType 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/entity/enums/PlatformType.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/entity/enums/PlatformType.java
@@ -1,5 +1,16 @@
 package com.SMWU.CarryUsServer.domain.member.entity.enums;
 
+import com.SMWU.CarryUsServer.domain.auth.exception.AuthException;
+
+import java.util.Arrays;
+
+import static com.SMWU.CarryUsServer.domain.auth.exception.AuthExceptionType.INVALID_PLATFORM_TYPE;
+
 public enum PlatformType {
-    KAKAO;
+    KAKAO, NAVER;
+
+    public static PlatformType of(String platformType) {
+        return Arrays.stream(PlatformType.values()).filter(type -> type.toString().equals(platformType)).findAny()
+                .orElseThrow(() -> new AuthException(INVALID_PLATFORM_TYPE));
+    }
 }
