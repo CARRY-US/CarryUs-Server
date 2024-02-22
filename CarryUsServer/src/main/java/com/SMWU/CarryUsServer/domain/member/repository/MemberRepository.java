79f6820a113cb1f800ae5df9ad2d151079075d33
sung-silver <jinna0319@gmail.com>
2024-02-23 00:47:44 +0900

    [FIX] memberRepository 이름 오타 수정

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepoistory extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Long, Member> {
     Optional<Member> findByPlatformTypeAndPlatformId(PlatformType platformType, String PlatformId);
 }
