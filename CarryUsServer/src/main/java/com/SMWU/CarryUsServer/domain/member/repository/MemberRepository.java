ba3509f75ed61b26f5e00b168882c482e851a639
sung-silver <jinna0319@gmail.com>
2024-02-23 18:07:30 +0900

    [FEAT] platformId, platformType에 대한 유니크 조건 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
-    Optional<Member> findByPlatformTypeAndPlatformId(PlatformType platformType, String PlatformId);
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByPlatformTypeAndPlatformId(PlatformType platformType, String platformId);
     Member saveAndFlush(Member member);
 }
