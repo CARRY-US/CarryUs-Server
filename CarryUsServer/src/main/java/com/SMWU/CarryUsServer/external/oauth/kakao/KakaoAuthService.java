01de0af532801b34df4998b317896e4dc9b06eb6
sung-silver <jinna0319@gmail.com>
2024-02-25 16:27:16 +0900

    [FIX] vo 필드 수정에 따른 수정

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/external/oauth/kakao/KakaoAuthService.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/external/oauth/kakao/KakaoAuthService.java
@@ -1,79 +1,57 @@
-package com.SMWU.CarryUsServer.domain.auth.service;
+package com.SMWU.CarryUsServer.external.oauth.kakao;
 
+import com.SMWU.CarryUsServer.domain.auth.controller.dto.enums.AuthType;
 import com.SMWU.CarryUsServer.domain.auth.controller.dto.request.MemberRequestDTO;
+import com.SMWU.CarryUsServer.domain.auth.service.AuthService;
 import com.SMWU.CarryUsServer.domain.auth.service.vo.MemberSignUpVO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.SMWU.CarryUsServer.domain.member.entity.Member;
+import com.SMWU.CarryUsServer.domain.member.entity.enums.PlatformType;
+import com.SMWU.CarryUsServer.domain.member.entity.enums.Role;
+import com.SMWU.CarryUsServer.domain.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-public class KakaoAuthService extends AuthService{
-    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+@Slf4j
+@Service
+public class KakaoAuthService extends AuthService {
+    @Value("${spring.security.oauth2.client.kakao.user-info-uri}")
     private String kakaoUserInfoUri;
 
-    private final KakaoMemberRepository kakaoMemberRepository;
-
-    public KakaoAuthService(
-            MemberRepository memberRepository, KakaoMemberRepository kakaoMemberRepository) {
+    public KakaoAuthService(MemberRepository memberRepository) {
         super(memberRepository);
-        this.kakaoMemberRepository = kakaoMemberRepository;
     }
 
     @Override
-    public SignedUpMemberVO saveMemberOrLogin(String platformToken, MemberSignUpRequestDTO request)
-            throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
+    public MemberSignUpVO saveMemberOrLogin(String platformToken, MemberRequestDTO request) {
+        KakaoMemberVO userInfoVO = getKakaoUserInfo(platformToken);
+        System.out.println("userInfoVO: " + userInfoVO.toString());
 
-        // 카카오 API로 사용자 정보 요청을 보낼 때 액세스 토큰을 Bearer 토큰 스키마로 전달하기 위한 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + platformToken);
-        // 헤더를 포함한 HttpEntity 생성
-        HttpEntity<String> entity = new HttpEntity<>(headers);
+        Member foundMember =
+                getMember(PlatformType.of(request.platformType()), userInfoVO.id().toString());
 
-        // HttpEntity를 사용하여 API 요청을 보내고 응답을 받음
-        // ResponseEntity<String> 타입으로 response를 받음
-        try {
-            String response =
-                    restTemplate.exchange(kakaoUserInfoUri, HttpMethod.GET, entity, String.class).getBody();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            // KakaoOAuthUserInfo 로 반환 받은 값으로 바로 회원가입되게 변경
-            KakaoUserVO userInfoVO = objectMapper.readValue(response, KakaoUserVO.class);
-            System.out.println("userInfoVO: " + userInfoVO.toString());
-
-            Member foundMember =
-                    getUser(request.getPlatformType(), userInfoVO.getKakaoAccount().getEmail());
-
-            if (foundMember != null) {
-                return SignedUpMemberVO.of(foundMember, null, AuthType.LOGIN);
-            }
-
-            Member savedMember = saveUser(request, userInfoVO.getKakaoAccount().getEmail());
-
-            if (!userInfoVO.getKakaoAccount().getAge_range_needs_agreement()
-                    || !userInfoVO.getKakaoAccount().getGender_needs_agreement()) {
-                saveKakaoMemberInfo(userInfoVO, savedMember);
-            }
-            return SignedUpMemberVO.of(savedMember, null, AuthType.SIGN_UP);
-
-        } catch (Exception e) {
-            throw new InvalidKakaoTokenException(
-                    ErrorType.INVALID_KAKAO_TOKEN_EXCEPTION,
-                    ErrorType.INVALID_KAKAO_TOKEN_EXCEPTION.getMessage());
+        if (foundMember != null) {
+            return MemberSignUpVO.of(foundMember, PlatformType.KAKAO, Role.of(request.role()), AuthType.LOGIN);
         }
+
+        Member savedMember = saveUser(request, userInfoVO.id().toString(), userInfoVO.kakaoAccount().profile().nickname(), null, userInfoVO.kakaoAccount().profile().thumbnailImageUrl());
+        return MemberSignUpVO.of(savedMember, PlatformType.KAKAO, Role.of(request.role()), AuthType.SIGN_UP);
     }
 
-    private void saveKakaoMemberInfo(KakaoUserVO kakaoUserVo, Member member) {
-        KakaoMember newKakaoMember =
-                KakaoMember.builder()
-                        .member(member)
-                        .gender(kakaoUserVo.getKakaoAccount().getGender())
-                        .age_range(kakaoUserVo.getKakaoAccount().getAge_range())
-                        .build();
-        kakaoMemberRepository.save(newKakaoMember);
+    private KakaoMemberVO getKakaoUserInfo(final String accessToken) {
+        WebClient webClient = WebClient.builder().build();
+        try {
+            return webClient.post()
+                    .uri(kakaoUserInfoUri)
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve()
+                    .bodyToMono(KakaoMemberVO.class)
+                    .block();
+        } catch (Exception e) {
+            log.error("getKakaoUserInfo error = {}", e);
+            throw new InvalidPropertyException(KakaoAuthService.class, "getKakaoUserInfo", "카카오 사용자 정보를 가져오는데 실패했습니다.");
+        }
     }
 }
