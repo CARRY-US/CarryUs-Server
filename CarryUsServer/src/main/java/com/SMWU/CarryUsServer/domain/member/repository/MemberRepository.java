ce75213807b945cca07368c818c3c2267e71899c
sung-silver <jinna0319@gmail.com>
2024-02-28 17:44:22 +0900

    [ADD] findByMemberIdOrElseThrowException 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/repository/MemberRepository.java
@@ -1,12 +1,22 @@
 package com.SMWU.CarryUsServer.domain.member.repository;
 
+import com.SMWU.CarryUsServer.domain.auth.exception.AuthException;
 import com.SMWU.CarryUsServer.domain.member.entity.Member;
 import com.SMWU.CarryUsServer.domain.member.entity.enums.PlatformType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+import static com.SMWU.CarryUsServer.domain.auth.exception.AuthExceptionType.NOT_FOUND_MEMBER;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByPlatformTypeAndPlatformId(PlatformType platformType, String platformId);
     Member saveAndFlush(Member member);
+
+    Optional<Member> findByMemberId(Long memberId);
+
+    default Member findByMemberIdOrElseThrowException(Long memberId){
+        return findByMemberId(memberId).orElseThrow(
+                () -> new AuthException(NOT_FOUND_MEMBER));
+    }
 }
