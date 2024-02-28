c93e57868bde63a8284c47781b39b7964617817c
sung-silver <jinna0319@gmail.com>
2024-02-28 17:44:39 +0900

    [FEAT] Role에 value 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/entity/enums/Role.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/entity/enums/Role.java
@@ -1,13 +1,18 @@
 package com.SMWU.CarryUsServer.domain.member.entity.enums;
 
 import com.SMWU.CarryUsServer.domain.auth.exception.AuthException;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 
 import static com.SMWU.CarryUsServer.domain.auth.exception.AuthExceptionType.INVALID_MEMBER_ROLE;
 
+@RequiredArgsConstructor
 public enum Role {
-    TRAVELER, STORE_OWNER;
+    TRAVELER("ROLE_TRAVELER"), STORE_OWNER("ROLE_STORE_OWNER");
+
+    private final String value;
+
     public static Role of(String role) {
         return Arrays.stream(Role.values()).filter(type -> type.toString().equals(role)).findAny()
                 .orElseThrow(() -> new AuthException(INVALID_MEMBER_ROLE));
