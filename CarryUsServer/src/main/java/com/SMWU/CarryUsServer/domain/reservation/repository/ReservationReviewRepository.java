4c391ff14968586186372454f25e64df8c9b93a4
sung-silver <jinna0319@gmail.com>
2024-03-02 01:00:03 +0900

    [FEAT] ReservationReviewRepository에 리뷰 id와 리뷰 작성자를 파라미터로 리뷰를 찾는 메서드 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationReviewRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationReviewRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationReviewRepository extends JpaRepository<ReservationReview, Long>{
     @Query("SELECT rr FROM ReservationReview rr JOIN FETCH rr.reservation r JOIN FETCH r.store WHERE rr.reservation.client = :client ORDER BY rr.createdAt DESC")
     List<ReservationReview> findAllByClient(@Param("client") Member client);
+
+    Optional<ReservationReview> findByReservationReviewIdAndReservationClient(Long reservationId, Member client);
 }
