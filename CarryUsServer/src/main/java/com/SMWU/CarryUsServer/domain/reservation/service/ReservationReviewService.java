617e85c49669f67a1a27d21262a81e5147250468
sung-silver <jinna0319@gmail.com>
2024-03-01 22:45:11 +0900

    [FEAT] 사용자가 작성한 리뷰 리스트를 가져오는 기능 개발

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/service/ReservationReviewService.java
@@ -0,0 +1,35 @@
+package com.SMWU.CarryUsServer.domain.reservation.service;
+
+import com.SMWU.CarryUsServer.domain.member.controller.response.MemberReviewResponseDTO;
+import com.SMWU.CarryUsServer.domain.member.entity.Member;
+import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
+import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationReview;
+import com.SMWU.CarryUsServer.domain.reservation.repository.ReservationRepository;
+import com.SMWU.CarryUsServer.domain.reservation.repository.ReservationReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationReviewService {
+    private final ReservationReviewRepository reservationReviewRepository;
+    private final ReservationRepository reservationRepository;
+
+    public List<MemberReviewResponseDTO> getMemberReviewList(final Member member) {
+        final List<ReservationReview> reservationReviews = reservationReviewRepository.findAllByClient(member);
+        List<MemberReviewResponseDTO> memberReviewListResponseDTO = new ArrayList<>();
+        for (ReservationReview reservationReview : reservationReviews) {
+            memberReviewListResponseDTO.add(MemberReviewResponseDTO.of(
+                    reservationReview.getReservationReviewId(),
+                    reservationReview.getReservation().getStore().getStoreName(),
+                    reservationReview.getReviewRating(),
+                    reservationReview.getReviewContent()));
+        }
+        return memberReviewListResponseDTO;
+    }
+
+}
