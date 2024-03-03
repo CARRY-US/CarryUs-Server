57958bf34334c3f4ec31c1523185baeba1b9f275
sung-silver <jinna0319@gmail.com>
2024-03-04 00:37:35 +0900

    [FEAT] 트랜잭션 적용

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/service/MemberService.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/service/MemberService.java
@@ -5,9 +5,11 @@ import com.SMWU.CarryUsServer.domain.member.entity.Member;
 import com.SMWU.CarryUsServer.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
 
