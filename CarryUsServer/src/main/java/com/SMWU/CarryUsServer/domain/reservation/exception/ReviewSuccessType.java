21532c7bd4b1cee4eaa18e0a5e9b16fad882a66b
sung-silver <jinna0319@gmail.com>
2024-03-04 00:41:03 +0900

    [ADD] 리뷰 수정에 따른 ReviewSuccessType 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReviewSuccessType.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReviewSuccessType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReviewSuccessType implements SuccessType {
     REVIEW_GET_SUCCESS(HttpStatus.OK, "리뷰 상세 조회에 성공하였습니다."),
-    REVIEW_STORE_INFO_GET_SUCCESS(HttpStatus.OK, "리뷰를 남긴 가게에 대한 정보 조회에 성공하였습니다.");
+    REVIEW_STORE_INFO_GET_SUCCESS(HttpStatus.OK, "리뷰를 남긴 가게에 대한 정보 조회에 성공하였습니다."),
+    REVIEW_UPDATE_SUCCESS(HttpStatus.OK, "리뷰 수정에 성공하였습니다.");
 
     private final HttpStatus status;
     private final String message;
