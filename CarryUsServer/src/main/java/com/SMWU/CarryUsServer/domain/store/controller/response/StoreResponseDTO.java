20e79876cc6e3d3acee6a89e2a6ca6b58112ccf8
sung-silver <jinna0319@gmail.com>
2024-03-05 20:24:10 +0900

    [FEAT] toStoreResponseDTO static 메서드 분리

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/controller/response/StoreResponseDTO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/controller/response/StoreResponseDTO.java
@@ -1,5 +1,7 @@
 package com.SMWU.CarryUsServer.domain.store.controller.response;
 
+import com.SMWU.CarryUsServer.domain.store.entity.Store;
+
 public record StoreResponseDTO(
         long storeId,
         String storeImgUrl,
@@ -11,5 +13,9 @@ public record StoreResponseDTO(
         double longitude) {
     public static StoreResponseDTO of(long storeId, String storeImgUrl, String storeName, String storeLocation, int storeReviewCount, double storeRatingAverage, double latitude, double longitude){
         return new StoreResponseDTO(storeId, storeImgUrl, storeName, storeLocation, storeReviewCount, storeRatingAverage, latitude, longitude);
+    }
+
+    public static StoreResponseDTO toStoreResponseDTO(final Store store, final int reviewCount, final double reviewRatingAverage) {
+        return StoreResponseDTO.of(store.getStoreId(), store.getStoreImgUrl(), store.getStoreName(), store.getStoreLocation(), reviewCount ,reviewRatingAverage , store.getLatitude(), store.getLongitude());
     }
 }
