1a7d2d37f30e9b27839e200742d42ae33591d213
sung-silver <jinna0319@gmail.com>
2024-03-06 13:29:06 +0900

    [FEAT] toMemberReviewResponseDTO 함수 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/controller/response/MemberReviewResponseDTO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/controller/response/MemberReviewResponseDTO.java
@@ -1,5 +1,7 @@
 package com.SMWU.CarryUsServer.domain.member.controller.response;
 
+import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationReview;
+
 public record MemberReviewResponseDTO(
         long reviewId,
         String storeName,
@@ -7,5 +9,9 @@ public record MemberReviewResponseDTO(
         String reviewContent) {
     public static MemberReviewResponseDTO of(long reviewId, String storeName, double reviewRating, String reviewContent) {
         return new MemberReviewResponseDTO(reviewId, storeName, reviewRating, reviewContent);
+    }
+
+    public static MemberReviewResponseDTO toMemberReviewResponseDTO(final ReservationReview reservationReview, final String storeName) {
+        return MemberReviewResponseDTO.of(reservationReview.getReservationReviewId(), storeName, reservationReview.getReviewRating(), reservationReview.getReviewContent());
     }
 }
