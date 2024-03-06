6c7d36c272c05e75d5433fa415732edeaf086e17
sung-silver <jinna0319@gmail.com>
2024-03-06 13:30:05 +0900

    [ADD] StoreReviewResponse 추가

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/StoreReviewResponseDTO.java
@@ -0,0 +1,26 @@
+package com.SMWU.CarryUsServer.domain.reservation.controller.response;
+
+import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationReview;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public record StoreReviewResponseDTO(
+        long reviewId,
+        String memberName,
+        String reviewCreatedAt,
+        double reviewRating,
+        String reviewText
+        ) {
+    public static StoreReviewResponseDTO of(long reviewId, String memberName, String reviewCreatedAt, double reviewRating, String reviewText) {
+        return new StoreReviewResponseDTO(reviewId, memberName, reviewCreatedAt, reviewRating, reviewText);
+    }
+
+    public static StoreReviewResponseDTO toStoreReviewResponseDTO(final ReservationReview reservationReview){
+        return StoreReviewResponseDTO.of(reservationReview.getReservationReviewId(), reservationReview.getReservation().getClient().getName(), getReviewCreatedAt(reservationReview.getCreatedAt()), reservationReview.getReviewRating(), reservationReview.getReviewContent());
+    }
+
+    private static String getReviewCreatedAt(final LocalDateTime createdAt) {
+        return createdAt.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+    }
+}
