7c0ed15ec92ebdb37282566104878e8037be725c
sung-silver <jinna0319@gmail.com>
2024-03-07 00:29:15 +0900

    [FEAT] 가게 상세 정보를 가져오는 서비스 로직 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/service/StoreService.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/service/StoreService.java
@@ -1,8 +1,12 @@
 package com.SMWU.CarryUsServer.domain.store.service;
 
 import com.SMWU.CarryUsServer.domain.reservation.repository.ReservationReviewRepository;
+import com.SMWU.CarryUsServer.domain.store.controller.response.BaggageTypeInfoResponseDTO;
+import com.SMWU.CarryUsServer.domain.store.controller.response.StoreDetailResponseDTO;
 import com.SMWU.CarryUsServer.domain.store.controller.response.StoreResponseDTO;
 import com.SMWU.CarryUsServer.domain.store.entity.Store;
+import com.SMWU.CarryUsServer.domain.store.entity.StoreBaggage;
+import com.SMWU.CarryUsServer.domain.store.repository.StoreBaggageRepository;
 import com.SMWU.CarryUsServer.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.SMWU.CarryUsServer.domain.store.controller.response.BaggageTypeInfoResponseDTO.getBaggageTypeInfoResponseDTO;
 import static com.SMWU.CarryUsServer.domain.store.controller.response.StoreResponseDTO.toStoreResponseDTO;
 
 @Service
@@ -19,6 +24,7 @@ import static com.SMWU.CarryUsServer.domain.store.controller.response.StoreRespo
 public class StoreService {
     private final StoreRepository storeRepository;
     private final ReservationReviewRepository reviewRepository;
+    private final StoreBaggageRepository storeBaggageRepository;
     public static final double EMPTY_RATING = 0.0;
 
     public List<StoreResponseDTO> getStoreListByMemberLocation(final double xMin, final double xMax, final double yMin, final double yMax) {
@@ -44,5 +50,20 @@ public class StoreService {
             responseDTOList.add(toStoreResponseDTO(store, reviewCount, reviewRating));
         }
         return responseDTOList;
+    }
+
+    public StoreDetailResponseDTO getStoreDetail(final long storeId) {
+        final Store store = storeRepository.findById(storeId).orElseThrow();
+        final List<BaggageTypeInfoResponseDTO> baggageTypeInfoList = getBaggageTypeInfoList(store);
+        return StoreDetailResponseDTO.getStoreDetailResponseDTO(store, baggageTypeInfoList);
+    }
+
+    private List<BaggageTypeInfoResponseDTO> getBaggageTypeInfoList(final Store store){
+        List<BaggageTypeInfoResponseDTO> baggageTypeInfoList = new ArrayList<>();
+        final List<StoreBaggage> storeBaggageList = storeBaggageRepository.findAllByStoreOrderByStoreBaggageIdAsc(store);
+        for(StoreBaggage storeBaggage : storeBaggageList){
+            baggageTypeInfoList.add(getBaggageTypeInfoResponseDTO(storeBaggage));
+        }
+        return baggageTypeInfoList;
     }
 }
