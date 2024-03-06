f3f7b0cb84fe9cbe0688991fe01fe122fb9effe1
sung-silver <jinna0319@gmail.com>
2024-03-06 13:29:41 +0900

    [FEAT] toReviewResponseDTO 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReviewResponseDTO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReviewResponseDTO.java
@@ -1,10 +1,16 @@
 package com.SMWU.CarryUsServer.domain.reservation.controller.response;
 
+import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationReview;
+
 public record ReviewResponseDTO(
         long reviewId,
         double reviewRating,
         String reviewContent) {
     public static ReviewResponseDTO of(long reviewId, double reviewRating, String reviewContent) {
         return new ReviewResponseDTO(reviewId, reviewRating, reviewContent);
+    }
+
+    public static ReviewResponseDTO toReviewResponseDTO(final ReservationReview reservationReview){
+        return ReviewResponseDTO.of(reservationReview.getReservationReviewId(), reservationReview.getReviewRating(), reservationReview.getReviewContent());
     }
 }
