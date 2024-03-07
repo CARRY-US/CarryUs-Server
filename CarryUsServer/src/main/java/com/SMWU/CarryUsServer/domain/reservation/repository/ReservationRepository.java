9fb2f93a8790ae512ef04f75277687a6baad2971
sung-silver <jinna0319@gmail.com>
2024-03-07 22:34:52 +0900

    [FEAT] ReservationType과 Client로 예약 리스트를 반환하는 쿼리 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationRepository.java
@@ -2,10 +2,12 @@ package com.SMWU.CarryUsServer.domain.reservation.repository;
 
 import com.SMWU.CarryUsServer.domain.member.entity.Member;
 import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
+import com.SMWU.CarryUsServer.domain.reservation.entity.enums.ReservationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -13,4 +15,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("SELECT r FROM Reservation r JOIN ReservationReview rr on r = rr.reservation JOIN FETCH r.store WHERE rr.reservationReviewId = :reviewId AND r.client = :client")
     Optional<Reservation> findReservationWithReservationStore(@Param("reviewId") Long reviewId, @Param("client") Member client);
+
+    List<Reservation> findAllByReservationTypeAndClientOrderByCreatedAtDesc(final ReservationType reservationType, final Member client);
 }
