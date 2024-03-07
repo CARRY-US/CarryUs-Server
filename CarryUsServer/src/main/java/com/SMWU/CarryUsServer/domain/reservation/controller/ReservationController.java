f1847ddab8dd29e0cec99991b9879ff5d861b6a6
sung-silver <jinna0319@gmail.com>
2024-03-07 14:50:11 +0900

    [FEAT] 예약 취소 컨트롤러 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/ReservationController.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/ReservationController.java
@@ -2,9 +2,12 @@ package com.SMWU.CarryUsServer.domain.reservation.controller;
 
 import com.SMWU.CarryUsServer.domain.member.entity.Member;
 import com.SMWU.CarryUsServer.domain.member.service.MemberService;
+import com.SMWU.CarryUsServer.domain.reservation.controller.request.ReservationCancelRequestDTO;
 import com.SMWU.CarryUsServer.domain.reservation.controller.request.ReviewCreateRequestDTO;
 import com.SMWU.CarryUsServer.domain.reservation.controller.response.MemberReservationDefaultInfoResponseDTO;
+import com.SMWU.CarryUsServer.domain.reservation.controller.response.ReservationIdResponseDTO;
 import com.SMWU.CarryUsServer.domain.reservation.controller.response.ReviewIdResponseDTO;
+import com.SMWU.CarryUsServer.domain.reservation.service.ReservationCancelService;
 import com.SMWU.CarryUsServer.domain.reservation.service.ReservationReviewService;
 import com.SMWU.CarryUsServer.global.response.SuccessResponse;
 import com.SMWU.CarryUsServer.global.security.AuthMember;
@@ -12,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.SMWU.CarryUsServer.domain.reservation.exception.ReservationSuccessType.RESERVATION_CANCEL_SUCCESS;
 import static com.SMWU.CarryUsServer.domain.reservation.exception.ReservationSuccessType.RESERVATION_MEMBER_DEFAULT_INFO_GET_SUCCESS;
 import static com.SMWU.CarryUsServer.domain.reservation.exception.ReviewSuccessType.REVIEW_CREATE_SUCCESS;
 
@@ -21,6 +25,7 @@ import static com.SMWU.CarryUsServer.domain.reservation.exception.ReviewSuccessT
 public class ReservationController {
     private final ReservationReviewService reservationReviewService;
     private final MemberService memberService;
+    private final ReservationCancelService reservationCancelService;
 
     @PostMapping("/{reservationId}/review")
     public ResponseEntity<SuccessResponse<ReviewIdResponseDTO>> createReview(@PathVariable("reservationId") final Long reservationId, @RequestBody final ReviewCreateRequestDTO requestDTO, @AuthMember final Member member) {
@@ -32,5 +37,11 @@ public class ReservationController {
     public ResponseEntity<SuccessResponse<MemberReservationDefaultInfoResponseDTO>> getMemberReservationDefaultInfo(@AuthMember final Member member) {
         final MemberReservationDefaultInfoResponseDTO responseDTO = memberService.getMemberReservationDefaultInfo(member);
         return ResponseEntity.ok(SuccessResponse.of(RESERVATION_MEMBER_DEFAULT_INFO_GET_SUCCESS, responseDTO));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<SuccessResponse<ReservationIdResponseDTO>> cancelReservation(@RequestBody final ReservationCancelRequestDTO request) {
+        final ReservationIdResponseDTO responseDTO = reservationCancelService.cancelReservation(request);
+        return ResponseEntity.ok(SuccessResponse.of(RESERVATION_CANCEL_SUCCESS, responseDTO));
     }
 }
