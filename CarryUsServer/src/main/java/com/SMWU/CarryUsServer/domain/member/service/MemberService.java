debe357a2ce78008d4deeb870cdf6386f23f0f26
sung-silver <jinna0319@gmail.com>
2024-03-07 12:54:35 +0900

    [FEAT] getMemberReservationDefaultInfo 서비스 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/service/MemberService.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.SMWU.CarryUsServer.domain.member.service;
 
 import com.SMWU.CarryUsServer.domain.member.controller.response.MemberProfileResponseDTO;
+import com.SMWU.CarryUsServer.domain.member.controller.response.MemberReservationDefaultInfoResponseDTO;
 import com.SMWU.CarryUsServer.domain.member.entity.Member;
 import com.SMWU.CarryUsServer.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
-    private final MemberRepository memberRepository;
-
     public MemberProfileResponseDTO getMemberProfile(final Member member) {
         return MemberProfileResponseDTO.of(member.getName(), member.getProfileImg());
+    }
+
+    public MemberReservationDefaultInfoResponseDTO getMemberReservationDefaultInfo(final Member member) {
+        return getMemberReservationDefaultInfo(member);
     }
 }
