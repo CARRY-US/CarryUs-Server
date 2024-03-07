f4a066afc0e906171e414756d8fcd76a1e3a58d3
sung-silver <jinna0319@gmail.com>
2024-03-07 22:34:07 +0900

    [ADD] ReservationFilteredByStatusResponseDTO 추가

---
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReservationFilteredByStatusResponseDTO.java
@@ -0,0 +1,10 @@
+package com.SMWU.CarryUsServer.domain.reservation.controller.response;
+
+import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
+import com.SMWU.CarryUsServer.domain.store.entity.Store;
+
+public record ReservationFilteredByStatusResponseDTO(long reservationId, String storeName, String storeImgUrl, String storeLocation, String reservationInfo) {
+    public static ReservationFilteredByStatusResponseDTO of(final Store store, final Reservation reservation) {
+        return new ReservationFilteredByStatusResponseDTO(reservation.getReservationId(), store.getStoreName(), store.getStoreImgUrl(), store.getStoreLocation(), reservation.getReservationInfo());
+    }
+}
