077f6aabbc7b90886c4f65a15d5e7e03df556291
sung-silver <jinna0319@gmail.com>
2024-03-07 13:32:21 +0900

    [REFACTOR] ReservationStoreInfoResponseDTO 리팩터링

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReservationStoreInfoResponseDTO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReservationStoreInfoResponseDTO.java
@@ -1,7 +1,9 @@
 package com.SMWU.CarryUsServer.domain.reservation.controller.response;
 
+import com.SMWU.CarryUsServer.domain.store.entity.Store;
+
 public record ReservationStoreInfoResponseDTO(long storeId, String storeName, String storeLocation, String reservationInfo) {
-    public static ReservationStoreInfoResponseDTO of(long storeId, String storeName, String storeLocation, String reservationInfo) {
-        return new ReservationStoreInfoResponseDTO(storeId, storeName, storeLocation, reservationInfo);
+    public static ReservationStoreInfoResponseDTO of(final Store store, final String reservationInfo) {
+        return new ReservationStoreInfoResponseDTO(store.getStoreId(), store.getStoreName(), store.getStoreLocation(), reservationInfo);
     }
 }
