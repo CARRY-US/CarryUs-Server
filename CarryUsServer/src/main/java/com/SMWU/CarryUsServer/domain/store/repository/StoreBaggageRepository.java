22cba66d713466445fd2a0ac59a6dd884f83c0aa
sung-silver <jinna0319@gmail.com>
2024-03-23 15:49:38 +0900

    [FEAT] StoreBaggageRepoistory - 가게와 가방 유형으로 찾는 쿼리 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/repository/StoreBaggageRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/store/repository/StoreBaggageRepository.java
@@ -2,10 +2,13 @@ package com.SMWU.CarryUsServer.domain.store.repository;
 
 import com.SMWU.CarryUsServer.domain.store.entity.Store;
 import com.SMWU.CarryUsServer.domain.store.entity.StoreBaggage;
+import com.SMWU.CarryUsServer.domain.store.entity.enums.BaggageType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoreBaggageRepository extends JpaRepository<StoreBaggage, Long> {
     List<StoreBaggage> findAllByStoreOrderByStoreBaggageIdAsc(Store store);
+    Optional<StoreBaggage> findByStoreAndBaggageType(Store store, BaggageType baggageType);
 }
