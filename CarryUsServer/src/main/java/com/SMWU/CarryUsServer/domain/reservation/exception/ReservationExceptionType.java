828fed74fde3310d5ab978eb75d8b64741a5928a
sung-silver <jinna0319@gmail.com>
2024-03-23 15:46:35 +0900

    [ADD] ReservationExceptionType에 시간대 별 캐리어 예약 개수 초과 메시지 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReservationExceptionType.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReservationExceptionType.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReservationExceptionType implements ExceptionType {
     NOT_COMPLETED_RESERVATION(HttpStatus.BAD_REQUEST, "예약이 완료되지 않았습니다."),
+    NOT_VALIDATE_RESERVATION_BAGGAGE(HttpStatus.BAD_REQUEST, "해당 시간대에 예약 가능한 캐리어의 개수를 초과했습니다."),
     NOT_FOUND_RESERVATION(HttpStatus.NOT_FOUND, "예약을 찾을 수 없습니다.");
 
     private final HttpStatus status;
