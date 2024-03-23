5131ecd419246f2d609f9207804d3d5f52e0ae92
sung-silver <jinna0319@gmail.com>
2024-03-23 15:46:06 +0900

    [ADD] ReservationSuccessType에 예약 생성 성공에 대한 타입 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReservationSuccessType.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/exception/ReservationSuccessType.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum ReservationSuccessType implements SuccessType {
     RESERVATION_MEMBER_DEFAULT_INFO_GET_SUCCESS(HttpStatus.OK, "회원의 기본 예약 정보 조회에 성공하였습니다."),
     RESERVATION_CANCEL_SUCCESS(HttpStatus.OK, "예약 취소에 성공하였습니다."),
-    RESERVATION_DETAIL_GET_SUCCESS(HttpStatus.OK, "예약 상세 정보 조회에 성공하였습니다.");
+    RESERVATION_DETAIL_GET_SUCCESS(HttpStatus.OK, "예약 상세 정보 조회에 성공하였습니다."),
+    RESERVATION_CREATE_SUCCESS(HttpStatus.CREATED, "예약 생성에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
