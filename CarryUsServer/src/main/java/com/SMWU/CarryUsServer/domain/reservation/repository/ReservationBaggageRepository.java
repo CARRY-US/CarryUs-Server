749cecc953562b7a210e72c80bc084d931118fbc
sung-silver <jinna0319@gmail.com>
2024-03-23 15:50:19 +0900

    [FEAT] ReservationBaggageRepository-시간대별 예약된 가방을 가져오는 쿼리 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationBaggageRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationBaggageRepository.java
@@ -2,10 +2,16 @@ package com.SMWU.CarryUsServer.domain.reservation.repository;
 
 import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
 import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationBaggage;
+import com.SMWU.CarryUsServer.domain.store.entity.StoreBaggage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationBaggageRepository extends JpaRepository<ReservationBaggage, Long> {
     List<ReservationBaggage> findAllByReservation(Reservation reservation);
+
+    @Query("select rb from ReservationBaggage rb where rb.storeBaggage = :storeBaggage and :time between rb.reservation.reservationStartTime and rb.reservation.reservationEndTime")
+    List<ReservationBaggage> findAllByStoreBaggageWithTime(StoreBaggage storeBaggage, LocalDateTime time);
 }
