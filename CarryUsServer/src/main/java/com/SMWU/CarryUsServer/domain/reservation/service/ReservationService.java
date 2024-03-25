3b654e4c828846d8acb1a77fd7687a559625e0f7
sung-silver <jinna0319@gmail.com>
2024-03-26 00:09:13 +0900

    [FEAT] ReservationService에 getReservationAvailableTime 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/service/ReservationService.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/service/ReservationService.java
@@ -7,16 +7,30 @@ import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationBaggage;
 import com.SMWU.CarryUsServer.domain.reservation.exception.ReservationException;
 import com.SMWU.CarryUsServer.domain.reservation.repository.ReservationBaggageRepository;
 import com.SMWU.CarryUsServer.domain.reservation.repository.ReservationRepository;
+import com.SMWU.CarryUsServer.domain.store.controller.response.ReservationAvailableTimeResponseDTO;
 import com.SMWU.CarryUsServer.domain.store.entity.Store;
+import com.SMWU.CarryUsServer.domain.store.entity.StoreBaggage;
+import com.SMWU.CarryUsServer.domain.store.entity.enums.BaggageType;
+import com.SMWU.CarryUsServer.domain.store.exception.StoreException;
 import com.SMWU.CarryUsServer.domain.store.repository.StoreBaggageRepository;
 import com.SMWU.CarryUsServer.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.SMWU.CarryUsServer.domain.reservation.exception.ReservationExceptionType.NOT_FOUND_RESERVATION;
+import static com.SMWU.CarryUsServer.domain.reservation.exception.ReservationExceptionType.NOT_VALIDATE_RESERVATION_BAGGAGE;
+import static com.SMWU.CarryUsServer.domain.store.exception.StoreExceptionType.NOT_FOUND_STORE;
+import static com.SMWU.CarryUsServer.domain.store.exception.StoreExceptionType.NOT_FOUND_STORE_BAGGAGE;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,6 +38,8 @@ import static com.SMWU.CarryUsServer.domain.reservation.exception.ReservationExc
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final ReservationBaggageRepository reservationBaggageRepository;
+    private final StoreRepository storeRepository;
+    private final StoreBaggageRepository storeBaggageRepository;
 
 
     public ReservationDetailResponseDTO getReservationDetail(final Long reservationId, final Member member){
@@ -54,5 +70,56 @@ public class ReservationService {
             payment += price*count;
         }
         return payment;
+    }
+
+    public ReservationAvailableTimeResponseDTO getReservationAvailableTime(final long storeId, final String date, final int extraSmallCount, final int smallCount, final int largeCount, final int extraLargeCount){
+        ConcurrentHashMap<BaggageType, Integer> baggageCountMap = BaggageType.createBaggageCountMap(extraSmallCount, smallCount, largeCount, extraLargeCount);
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new StoreException(NOT_FOUND_STORE));
+        LocalDateTime reservationDate = LocalDateTime.parse(date + "T00:00:00");
+        final List<Boolean> response = getAvailableTime(baggageCountMap, store, reservationDate);
+        return ReservationAvailableTimeResponseDTO.of(response);
+    }
+
+    private List<Boolean> getAvailableTime(final ConcurrentHashMap<BaggageType, Integer> baggageCount, final Store store, final LocalDateTime reservationDate) {
+        List<Boolean> availableTimeList = new ArrayList<>();
+        LocalDateTime startTime = getStartTime(store, reservationDate);
+
+        for (int i = 0; i < 24; i++) {
+            LocalDateTime currentTime = reservationDate.plusHours(i);
+            if (currentTime.isBefore(startTime)) {
+                availableTimeList.add(false);
+                continue;
+            }
+            boolean available = isAvailableForBaggageType(baggageCount, store, currentTime);
+            availableTimeList.add(available);
+        }
+        return availableTimeList;
+    }
+
+    private boolean isAvailableForBaggageType(final ConcurrentHashMap<BaggageType, Integer> baggageCount, final Store store, final LocalDateTime currentTime) {
+        for (BaggageType baggageType : BaggageType.values()) {
+            int count = baggageCount.get(baggageType);
+            StoreBaggage storeBaggage = storeBaggageRepository.findByStoreAndBaggageType(store, baggageType)
+                    .orElseThrow(() -> new StoreException(NOT_FOUND_STORE_BAGGAGE));
+            int reservedCount = reservationBaggageRepository.findAllByStoreBaggageWithTime(storeBaggage, currentTime)
+                    .stream()
+                    .mapToInt(ReservationBaggage::getReservationBaggageCount)
+                    .sum();
+            if (count + reservedCount > storeBaggage.getBaggageCount()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private LocalDateTime getStartTime(final Store store, final LocalDateTime reservationDate){
+        Pattern pattern = Pattern.compile("\\b\\d{2}:\\d{2}\\b");
+        Matcher matcher = pattern.matcher(store.getOpeningHour());
+        LocalTime startTime = null;
+        if (matcher.find()) {
+            String timeString = matcher.group();
+            startTime = LocalTime.parse(timeString);
+        }
+        return LocalDateTime.of(reservationDate.toLocalDate(), Objects.requireNonNull(startTime));
     }
 }
