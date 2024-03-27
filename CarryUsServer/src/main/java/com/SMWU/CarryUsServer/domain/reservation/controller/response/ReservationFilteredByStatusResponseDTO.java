b0304fc310e4ddfcdc0eb7a0caee0c5d73166a98
sung-silver <jinna0319@gmail.com>
2024-03-27 17:35:46 +0900

    [ADD] isReviewExist 필드 추가

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReservationFilteredByStatusResponseDTO.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/controller/response/ReservationFilteredByStatusResponseDTO.java
@@ -3,8 +3,8 @@ package com.SMWU.CarryUsServer.domain.reservation.controller.response;
 import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
 import com.SMWU.CarryUsServer.domain.store.entity.Store;
 
-public record ReservationFilteredByStatusResponseDTO(long reservationId, String storeName, String storeImgUrl, String storeLocation, String reservationInfo) {
-    public static ReservationFilteredByStatusResponseDTO of(final Store store, final Reservation reservation) {
-        return new ReservationFilteredByStatusResponseDTO(reservation.getReservationId(), store.getStoreName(), store.getStoreImgUrl(), store.getStoreLocation(), reservation.getReservationInfo());
+public record ReservationFilteredByStatusResponseDTO(long reservationId, String storeName, String storeImgUrl, String storeLocation, String reservationInfo, boolean isReviewExist) {
+    public static ReservationFilteredByStatusResponseDTO of(final Store store, final Reservation reservation, boolean isReviewExist) {
+        return new ReservationFilteredByStatusResponseDTO(reservation.getReservationId(), store.getStoreName(), store.getStoreImgUrl(), store.getStoreLocation(), reservation.getReservationInfo(), isReviewExist);
     }
 }
