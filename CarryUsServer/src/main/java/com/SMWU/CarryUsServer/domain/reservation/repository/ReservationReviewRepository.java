a5a01bb59fe4856ecd38b0604168511fff4d10af
sung-silver <jinna0319@gmail.com>
2024-03-27 17:39:02 +0900

    [FEAT] ReservationReviewRepository에 existsByReservation 구현

--- CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationReviewRepository.java
+++ CarryUsServer/src/main/java/com/SMWU/CarryUsServer/domain/reservation/repository/ReservationReviewRepository.java
@@ -1,6 +1,7 @@
 package com.SMWU.CarryUsServer.domain.reservation.repository;
 
 import com.SMWU.CarryUsServer.domain.member.entity.Member;
+import com.SMWU.CarryUsServer.domain.reservation.entity.Reservation;
 import com.SMWU.CarryUsServer.domain.reservation.entity.ReservationReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -23,4 +24,6 @@ public interface ReservationReviewRepository extends JpaRepository<ReservationRe
 
     @Query("SELECT rr FROM ReservationReview rr JOIN FETCH rr.reservation r WHERE r.store.storeId =:storeId")
     List<ReservationReview> getReservationReviewByStore(@Param("storeId") Long storeId);
+
+    boolean existsByReservation(Reservation reservation);
 }
